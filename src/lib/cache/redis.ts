a898458fb358dc1fe6f48377a8bf5fa93a56b5d8
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-06-02 20:28:33 -0400

    rename environment variable

--- src/lib/cache/redis.ts
+++ src/lib/cache/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 import type {} from 'ioredis';
-import { REDIS_ENDPOINT_URL, REDIS_PASSWORD, REDIS_USERNAME } from '$env/static/private';
+import { REDIS_ENDPOINT_URI, REDIS_PASSWORD, REDIS_USERNAME } from '$env/static/private';
 
 export const WEATHER_COORDS_KEY = 'weather_coords';
 
@@ -8,7 +8,7 @@ export let redis: any = null;
 
 try {
 	redis = new Redis({
-		host: REDIS_ENDPOINT_URL,
+		host: REDIS_ENDPOINT_URI,
 		name: REDIS_USERNAME,
 		password: REDIS_PASSWORD,
 		port: 10772,
